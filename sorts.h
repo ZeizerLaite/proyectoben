b677a516a5925383bbcaab88a8b72c6b9d560a5b
ZeizerLaite <gabito.alf.sal@gmail.com>
2026-09-08 10:32:59 -0600

    Added the new things

---
+++ sorts.h
@@ -0,0 +1,15 @@
+#ifndef SORTS
+#define SORTS
+#include <iostream>
+#include <vector>
+
+template <class T>
+class sorting 
+{
+    public:
+    void quickSort(std::vector<T>);
+    
+};
+
+
+#endif
